ac6dcff6da53bd9e90395b625e4a41bdf03f9b06
Jan Piskvor Martinec <github@piskvor.org>
2017-12-06 17:16:39 +0100

    create/edit blog article

--- blog/src/AppBundle/Controller/PrivateController.php
+++ blog/src/AppBundle/Controller/PrivateController.php
@@ -1,29 +1,30 @@
 <?php
 declare(strict_types=1);
 
-
 namespace AppBundle\Controller;
 
+use AdamQuaile\Bundle\FieldsetBundle\Form\FieldsetType;
 use /** @noinspection PhpUnusedAliasInspection - used by annotations */
     Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use AppBundle\Entity\BlogArticle;
+use Symfony\Component\Form\Form;
+use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
-use Symfony\Component\Form\Extension\Core\Type\RadioType;
-use Symfony\Component\Form\Form;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use \DateTime;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\FormBuilder;
 
 class PrivateController extends Controller
 {
@@ -35,6 +36,7 @@ class PrivateController extends Controller
      */
     public function createAction(Request $request)
     {
+
         $blogArticle = new BlogArticle();
         $blogArticle->setArticleDate(new DateTime());
         $blogArticle->setArticleShown(false);
@@ -42,7 +44,7 @@ class PrivateController extends Controller
         $blogArticle->setUrl('');
         $blogArticle->setArticleText('Lorem ipsum dolor sit amet...');
 
-        /*return $this->editAction($request, $blogArticle);
+        return $this->formAction($request, $blogArticle);
 
     }
 
@@ -50,43 +52,79 @@ class PrivateController extends Controller
      * @Route("/private/edit", name="edit")
      * @param Request $request
      * @return Response
-     *
-    public function editAction(Request $request, $blogArticle)
+     */
+    public function editAction(Request $request)
     {
-        */
-//        var_dump($request);exit;
+        $blogArticleId = (int)$request->query->get('id');
+
+        /** @var BlogArticle $blogArticle */
+        $blogArticle = $this->getDoctrine()
+            ->getRepository(BlogArticle::class)
+            ->find($blogArticleId);
+
+        if (!$blogArticle) {
+            throw $this->createNotFoundException(
+                'No article found for id ' . $blogArticleId
+            );
+        }
+        return $this->formAction($request, $blogArticle);
+    }
+
+    /**
+     * @param Request $request
+     * @param BlogArticle $blogArticle
+     * @return Response
+     */
+    public function formAction(Request $request, BlogArticle $blogArticle)
+    {
+
         /** @var FormBuilder $form */
         $formBuilder = $this->createFormBuilder($blogArticle);
-        $formBuilder->add('title', TextType::class)
-            ->add('url', TextType::class)
-            ->add('articleDate', DateType::class)
-            ->add('articleShown', ChoiceType::class, array('choices' => array("Hidden" => 0, "Published" => 1), 'expanded' => true))
-            ->add('articleText', TextareaType::class)
-            ->add('save', SubmitType::class, array('label' => 'create article'));
         if ($blogArticle->getId()) {
-            $form->
-            add('id', TextType::class);
+            $actionName = 'update';
+            $formBuilder->add('id', HiddenType::class);
+        } else {
+            $actionName = 'create';
         }
+        $formBuilder->add('title', TextType::class, array(
+            'attr' => array(
+                'class' => 'long-text')))
+            ->add('url', TextType::class, array(
+                'attr' => array(
+                    'class' => 'long-text')))
+            ->add('articleDate', DateType::class, array(
+                'widget' => 'single_text',
+                'format' => 'yyyy-MM-dd',
+            ))
+            ->add('articleShown', ChoiceType::class, array(
+                'choices' => array("Hidden" => 0, "Published" => 1), 'expanded' => true))
+            ->add('articleText', TextareaType::class)
+            ->add('buttons', FieldsetType::class, [
+                'label' => false,
+                'legend' => '',
+                'fields' => function (FormBuilderInterface $fbuilder) use ($actionName) {
+                    $fbuilder
+                        ->add('save', SubmitType::class, array('label' => $actionName . ' article', 'attr' => array('class' => 'button-' . $actionName)))
+                        ->add('cancel', ButtonType::class, array('label' => 'back to list', 'attr' => array('class' => 'button-cancel')));
+                }
+            ]);;
 
         /** @var Form $form */
         $form = $formBuilder->getForm();
-        //var_export($form->isSubmitted());exit;
-        if ($form->isSubmitted()) {
-            if( $form->isValid()) {
-                $blogArticle = $form->getData();
+        $form->handleRequest($request);
 
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($blogArticle);
-                $em->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $blogArticle = $form->getData();
 
-                var_dump($blogArticle->getId());exit;
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($blogArticle);
+            $em->flush();
+
+            if ($blogArticle->getId()) {
                 return $this->redirectToRoute('edit', array('id' => $blogArticle->getId()));
             }
-             else {
-                var_dump($form->getErrors());exit;
-             }
         }
-        return $this->render('private/create.html.twig', array('form' => $form->createView()));
+        return $this->render('private/baseForm.html.twig', array('form' => $form->createView()));
 
     }
 
